058a497c880897ceb6b2139756f2f4ca37c187af
Christian Tietze <me@christiantietze.de>
2025-12-30 09:10:44 +0100

    chore: add project configs and tasks

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((prog-mode . ((fill-column . 100)
+			   (eval . (progn (indent-tabs-mode 1)
+							  (add-hook 'before-save-hook
+										(lambda () (when eglot--managed-mode (eglot-format-buffer)))
+										nil t)))))
+ (swift-ts-mode . ((eval . (progn (eglot-ensure))))))
